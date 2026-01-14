9b5a65acc49a9110f397670e394138b68a6fdbd6
Kenza Zulfiquar <153430286+kz2525@users.noreply.github.com>
2026-01-14 06:13:38 -0500

    Add files via upload

---
+++ farm/farms/doctype/manage_stall/manage_stall.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, kenza and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Manage Stall", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ farm/farms/doctype/manage_stall/test_manage_stall.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, kenza and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestManageStall(FrappeTestCase):
+	pass
